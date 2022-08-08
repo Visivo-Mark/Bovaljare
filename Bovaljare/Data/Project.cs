bdb8548e3c4c6ddf50209f648bc3d96334c4a78b
Mark Tibblin <mark.tibblin@gmail.com>
2022-08-08 10:05:28 +0200

    Added Project class to keep track on project-specific data.

---
+++ Bovaljare/Data/Project.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Bovaljare.Util;
+
+namespace Bovaljare.Data
+{
+  public class Project
+  {
+    private readonly static Dictionary<string, Project> Projects = new();
+
+    public string Name { get; set; }
+    public bool IsSunStudy { get; set; } = false;
+    public bool DisplayRent { get; set; } = false;
+    public Dictionary<string, double> GPS { get; set; }
+
+    public static Project GetProject(string uri)
+    {
+      if (!Projects.ContainsKey(uri)) {
+        string json = FileHandler.GetContents(@"wwwroot\data\projects\" + uri + ".json");
+        Projects.Add(uri, JsonConvert.DeserializeObject<Project>(json));
+      }
+
+      return Projects[uri];
+    }
+  }
+}
